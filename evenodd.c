fdf881b77d3f53dc756e4d03d624a75675748e5b
PayalPadmalyaPrusty <98264541+PayalPadmalyaPrusty@users.noreply.github.com>
2022-05-02 01:09:52 +0530

    assignment-03 done

---
+++ evenodd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int check(int num)
+{
+    return !(num & 1);
+}
+
+
+int main()
+{
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+    
+    if(check(num))
+    {
+        printf("The number is even.");
+    }
+    else
+    {
+        printf("The number is odd.");
+    }
+    
+    return 0;
+}
